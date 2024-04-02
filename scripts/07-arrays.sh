a9ac11ee9527f6a62d8f29e0b6d67b8186acc0e3
PraveenKumar8919 <ppattiri@aum.edu>
2024-04-01 21:04:11 -0500

    adding third file

---
+++ scripts/07-arrays.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NAMES=("Praveen" "Kumar" "PNS")
+
+echo "First name in the array is: ${NAMES[0]}"
+echo "Second name in the array is: ${NAMES[1]}"
+echo "Third name in the array is: ${NAMES[2]}"
+
+echo "Getting all names in the array:: ${NAMES[@]}"
+
+echo "this is the end of the code::"
+
