f343c895115a4b9329175f0dcf739d23737f96ab
PraveenKumar8919 <ppattiri@aum.edu>
2024-04-02 11:07:07 -0500

    Syntax error modified

--- scripts/10-functions.sh
+++ scripts/10-functions.sh
@@ -4,19 +4,19 @@ DATE=$(date)
 echo "Script executed on :: $DATE"
 
 VALIDATE(){
-    if [ $? -ne 0 ]
+    if [ $1 -ne 0 ]
     then
-        echo "Installation of git is not successfull"
+        echo "$2....failure"
         exit 1
     else
-        echo "Installation of git is successfull"
+        echo "$2.... sucess"
     fi
 }
 
 yum install mysql -y
-VALIDATE
+VALIDATE $? "Installation of mysql"
 
 yum install git -y
-VALIDATE
+VALIDATE $? "Installation of git"
 
 echo "This is end of the programm"
