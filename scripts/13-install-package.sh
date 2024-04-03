1c75cf134763915a1b3ecd19d064f1489d341c3f
PraveenKumar8919 <ppattiri@aum.edu>
2024-04-03 18:57:27 -0500

    uploading all

--- scripts/13-install-package.sh
+++ scripts/13-install-package.sh
@@ -3,7 +3,18 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2.. $R ERROR $N"
+    else
+        echo -e "$2 .. $G SUCCESS $N"
+    fi
+}
 
 if [ $ID -ne 0 ]
 then
@@ -14,3 +25,15 @@ else
 fi
 
 #echo "All arguments passed $@"
+
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y
+        VALIDATE $? "Installation of $package"
+    else
+        echo -e "$package is already installed.. $Y skipping package $N"
+        
+done
