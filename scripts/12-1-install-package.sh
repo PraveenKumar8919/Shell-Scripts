9501a882739b9d1695761f7822ea1fcf7d98d0e3
PraveenKumar8919 <ppattiri@aum.edu>
2024-04-03 19:28:32 -0500

    uploading all

---
+++ scripts/12-1-install-package.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "The script executed on $TIMESTAMP" &>> $LOGGFILE
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2.. $R ERROR $N"
+    else
+        echo -e "$2 .. $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e " $R Please run the script as a root user:: $N"
+    exit 1
+else
+    echo "You are a root user::.."
+fi
+
+#echo "All arguments passed $@"
+
+for package in $@
+do
+    yum list installed $package &>> $LOGGFILE
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y &>> $LOGGFILE
+        VALIDATE $? "Installation of $package"
+    else
+        echo -e "$package is already installed.. $Y skipping package $N"
+    fi     
+done
